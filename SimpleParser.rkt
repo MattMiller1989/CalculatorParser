c0074ec203073700295a26b4bd920f046ac01d7b
mattmiller1989 <mattmiller0989@gmail.com>
2021-10-02 22:46:56 -0500

    First methods

--- SimpleParser.rkt
+++ SimpleParser.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+(define (parse inputFile)
+  (scan inputFile )
+  )
+
+(define (scan inputFile)
+  (define file-contents
+    (port->string (open-input-file inputFile) #:close? #t))
+  write file-contents
+  
+  )
+
