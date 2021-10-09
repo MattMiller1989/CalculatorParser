38b2599ac71f99306089192edba405221f1c2980
mattmiller1989 <mattmiller0989@gmail.com>
2021-10-09 01:13:03 -0500

    Added error check

--- SimpleParser.rkt
+++ SimpleParser.rkt
@@ -3,7 +3,7 @@
 
 (define (parse inputFile)
   
-  (display (scan(remove-white-space (scan-prep inputFile) ) 0))
+  (display (scan(remove-white-space (scan-prep inputFile) ) 1))
   ;(remove-white-space (scan-prep inputFile) )
   )
 
@@ -15,22 +15,34 @@
 
 ;https://stackoverflow.com/questions/34981123/how-to-parse-this-string-in-scheme
 (define (remove-white-space file-contents)  
-  (string-split (string-replace (string-replace file-contents "(" "( " )  ")" " )" ))
+  (string-split (string-replace (string-replace (string-replace file-contents "(" "( " )  ")" " )" ) "\n" "newLine "))
  )
 
 (define (scan element-list line)
   
-  (if (string=? "$$" (first element-list))
-       "\nDone Scanning" 
-      (string-append (process (first element-list) line) " " (scan (rest element-list) line)))
-  )
+  ;(if (string=? "$$" (first element-list))
+   ;    "\nDone Scanning" 
+    ;  ((if (string=? "newLine" (first element-list))
+     ; (scan (rest element-list) (+ line 1))
+      ; (string-append (process (first element-list) line) " " (scan (rest element-list) line))))
+  ;)
+  (cond
+    [(string=? "$$" (first element-list)) "\nDone Scanning\n"]
+    [(string=? "newLine" (first element-list)) (scan (rest element-list) (+ line 1))]
+    [else (string-append (process (first element-list) line) " " (scan (rest element-list) line))]
+  ))
 
 (define (process element line)
   (cond
-    [(is-read? element) element]
+    [(is-read? element) element ]
     [(is-write? element) element]
     [(is-num? element) "num"]
-    [else element]
+    [(is-id? element) "id"]
+    [(is-l-paren? element) "lParen"]
+    [(is-r-paren? element) "rParen"]
+    [(is-mult-op? element) "multOp"]
+    [(is-add-op? element) "addOp"]   
+    [else (string-append "Error on line " (~a line))]
   ))
 
 (define (is-read? element)
@@ -43,6 +55,27 @@
 
 (define (is-num? element)
   (regexp-match? #rx"^[0-9.]+$" element)
+  )
+
+;https://stackoverflow.com/questions/17564088/how-to-form-a-regex-to-recognize-correct-declaration-of-variable-names/17564142
+(define (is-id? element)
+  (regexp-match? #rx"^[a-zA-Z_][a-zA-Z_0-9]*$" element)
+  )
+
+(define (is-l-paren? element)
+  (regexp-match? #rx"^\\($" element)
+  )
+
+(define (is-r-paren? element)
+  (regexp-match? #rx"^\\)$" element)
+  )
+
+(define (is-mult-op? element)
+  (regexp-match? #rx"(^\\*$)|(^/$)" element)
+  )
+
+(define (is-add-op? element)
+  (regexp-match? #rx"(^\\+$)|(^-$)" element)
   )
   
   
