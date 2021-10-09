d41a0f69e244ddfcb8f7da1340a5796c11ffaeed
mattmiller1989 <mattmiller0989@gmail.com>
2021-10-08 22:10:30 -0500

    Got recursion to work :)

--- SimpleParser.rkt
+++ SimpleParser.rkt
@@ -1,13 +1,36 @@
 #lang racket
+(define file "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input01.txt" )
 
 (define (parse inputFile)
-  (scan inputFile )
+  
+  (scan(remove-white-space (scan-prep inputFile) ))
   )
 
-(define (scan inputFile)
-  (define file-contents
-    (port->string (open-input-file inputFile) #:close? #t))
-  write file-contents
+(define (scan-prep inputFile)
+  
+  (port->string (open-input-file inputFile) #:close? #t)
   
   )
 
+;https://stackoverflow.com/questions/34981123/how-to-parse-this-string-in-scheme
+(define (remove-white-space file-contents)
+  
+  (string-split file-contents)  
+ )
+
+(define (scan element-list)
+  
+  (if (empty? element-list)
+      (write "Done Scanning \n" )
+      (process element-list)
+  ))
+
+(define (process element-list)
+  (display (first element-list))
+  (display " ")
+  (scan (rest element-list))
+  )
+  
+  
+  
+                          
