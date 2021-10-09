59bf6b7ecd86cea24f6331d29523ab8e876472a9
mattmiller1989 <mattmiller0989@gmail.com>
2021-10-08 23:56:47 -0500

    Added check for number

--- SimpleParser.rkt
+++ SimpleParser.rkt
@@ -3,7 +3,8 @@
 
 (define (parse inputFile)
   
-  (display (scan(remove-white-space (scan-prep inputFile) )))
+  (display (scan(remove-white-space (scan-prep inputFile) ) 0))
+  ;(remove-white-space (scan-prep inputFile) )
   )
 
 (define (scan-prep inputFile)
@@ -13,22 +14,38 @@
   )
 
 ;https://stackoverflow.com/questions/34981123/how-to-parse-this-string-in-scheme
-(define (remove-white-space file-contents)
-  
-  (string-split file-contents)  
+(define (remove-white-space file-contents)  
+  (string-split (string-replace (string-replace file-contents "(" "( " )  ")" " )" ))
  )
 
-(define (scan element-list)
+(define (scan element-list line)
   
   (if (string=? "$$" (first element-list))
-       "\nDone Scanning!!!!!!" 
-      (string-append (process (first element-list)) " " (scan (rest element-list))))
+       "\nDone Scanning" 
+      (string-append (process (first element-list) line) " " (scan (rest element-list) line)))
   )
 
-(define (process element)
-  
-   element
+(define (process element line)
+  (cond
+    [(is-read? element) element]
+    [(is-write? element) element]
+    [(is-num? element) "num"]
+    [else element]
+  ))
+
+(define (is-read? element)
+  (regexp-match? #rx"^(read)$" element)
   )
+
+(define (is-write? element)
+  (regexp-match? #rx"^(write)$" element)
+  )
+
+(define (is-num? element)
+  (regexp-match? #rx"^[0-9.]+$" element)
+  )
+  
+  
   
   
   
