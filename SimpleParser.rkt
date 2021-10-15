d3f20fc9bfe5df7854fb0a20248428bed11b79a7
mattmiller1989 <mattmiller0989@gmail.com>
2021-10-15 01:17:54 -0500

    DONE WITH THIS SHIT!!!

--- SimpleParser.rkt
+++ SimpleParser.rkt
@@ -1,19 +1,28 @@
- #lang racket
-(define file1 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input01.txt" )
-(define file2 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input02.txt" )
-(define file3 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input03.txt" )
-(define file4 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input04.txt" )
-(define file5 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input05.txt" )
-(define file6 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input06.txt" )
+#lang racket
 
-(define (parse inputFile)
+(require racket/trace)
+;(define file1 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input01.txt" )
+;(define file2 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input02.txt" )
+;(define file3 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input03.txt" )
+;(define file4 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input04.txt" )
+;(define file5 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input05.txt" )
+;(define file6 "C:/Users/Matt/Desktop/Fall21/CS441/ParserProject/ParserInputFiles/Input06.txt" )
+
+(define file1 "Input01.txt" )
+(define file2 "Input02.txt" )
+(define file3 "Input03.txt" )
+(define file4 "Input04.txt" )
+(define file5 "Input05.txt" )
+(define file6 "Input06.txt" )
+
+(define(parse inputFile)
   ;(display (scan(remove-white-space (scan-prep inputFile) ) 1))
   (define tokens (scan(remove-white-space (scan-prep inputFile) ) 1))
   (define token-stream (clean-tokens tokens))
   ;(display token-stream)
   (cond
     [(string-contains? tokens "Error") tokens]
-    [else (parse-token token-stream (list "program") 1)]
+    [else (program token-stream 1)]
   )
   
   )
@@ -56,7 +65,7 @@
     [(is-add-op? element) "add"]
     [(is-minus-op? element) "minus"]
     [(is-assign? element) "assign"]
-    [else (string-append "\n Scan Error on line " (~a line))]
+    [else (error (string-append "Scan Error on line " (~a line)))]
   ))
 
 (define (is-read? element)
@@ -102,170 +111,130 @@
   (regexp-match? #rx"^(==)$" element)
   )
   
-(define (parse-token token-stream call-stack line)
-  ;(define top-of-stack (first (call-stack)))
-  (display (string-append (first token-stream) (first call-stack) "\n"))
-  (cond
-    [(string=? (first call-stack) "ERROR") (string-append "\n Parse Error on line " (~a line))]
-    [(string=? (first call-stack) "program") (program token-stream call-stack line)]
-    [(string=? (first call-stack) "stmt_list") (stmt_list token-stream call-stack line)]
-    [(string=? (first call-stack) "stmt") (stmt token-stream call-stack line)]
-    [(string=? (first call-stack) "expr") (expr token-stream call-stack line)]
-    [(string=? (first call-stack) "term_tail") (term_tail token-stream call-stack line)]
-    [(string=? (first call-stack) "term") (term token-stream call-stack line)]
-    [(string=? (first call-stack) "factor_tail") (factor_tail token-stream call-stack line)]
-    [(string=? (first call-stack) "factor") (factor token-stream call-stack line)]
-    [(string=? (first call-stack) "add_op") (add_op token-stream call-stack line)]
-    [(string=? (first call-stack) "mult_op") (mult_op token-stream call-stack line)]
-    [(empty? call-stack) "\n file parsed successfully"]
-    [else `("this sucks")]
-    )
-)
 
-(define (program token-stream call-stack line)
+
+(define (program token-stream line)
   (define curr-token (first token-stream))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
   (cond
-    [(string=? curr-token "id") (match (parse-token token-stream (append `("stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "read") (match (parse-token token-stream (append `("stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "write") (match (parse-token token-stream (append `("stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "$$") (match (parse-token token-stream (append popped-stack `("stmt_list")) line) "$$")]
-    [else (parse-token (rest token-stream) `("PROGRAM ERROR") line)]
+    [(string=? curr-token "id") (success (match (stmt_list token-stream line) "$$"))]
+    [(string=? curr-token "read") (success (match (stmt_list token-stream line) "$$"))]
+    [(string=? curr-token "write") (success (match (stmt_list token-stream line) "$$"))]
+    [(string=? curr-token "$$") (success (match (stmt_list token-stream line) "$$"))]
+    [else (error(string-append "ERROR ON LINE" (~a line)))]
     )
-
   )
 
-(define (stmt_list token-stream call-stack line)
+(define (stmt_list token-stream line)
   (define curr-token (first token-stream))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
-  (cond
-    [(string=? curr-token "id") (match (parse-token token-stream (append `("stmt" "stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "read") (match (parse-token token-stream (append `("stmt" "stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "write") (match (parse-token token-stream (append `("stmt" "stmt_list") popped-stack ) line) "$$")]
-    [(string=? curr-token "$$") (parse-token (token-stream) call-stack line)]
-    [(string=? curr-token "newLine") (stmt_list (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `("STMT LIST ERROR") line)]
+    (cond
+    [(string=? curr-token "id") (stmt_list (stmt token-stream line) line)  ]
+    [(string=? curr-token "read") (stmt_list (stmt token-stream line) line ) ]
+    [(string=? curr-token "write") (stmt_list (stmt token-stream line) line ) ]
+    [(string=? curr-token "$$") token-stream]
+    [(string=? curr-token "newLine") (stmt_list (rest token-stream)  (+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line))) ]
     ))
 
-(define (stmt token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define third-token (first(rest (rest token-stream))))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+
+
+(define (stmt token-stream line)
+  (define curr-token (first token-stream)) 
   (cond
-    [(string=? curr-token "id")(parse-token (match (match token-stream "id") "assign")(append `("expr") popped-stack ) line)]
-    [(string=? curr-token "read") (parse-token (match (match token-stream "read") "id") popped-stack line)]
-    [(string=? curr-token "write") (parse-token (match token-stream "write") (append `("expr") popped-stack ) line)]
-    [(string=? curr-token "newLine") (stmt (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `("STMT ERROR") line)]
+    [(string=? curr-token "id")(expr (match (match token-stream "id") "assign") line)]
+    [(string=? curr-token "read") (match (match token-stream "read" ) "id" )]
+    [(string=? curr-token "write") (expr (match token-stream "write" ) line)]
+    [(string=? curr-token "newLine") (stmt (rest token-stream) (+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (expr token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (expr token-stream line)
+  (define curr-token (first token-stream))  
   (cond
-    [(string=? curr-token "lParen") (parse-token token-stream (append `("term","term_tail") popped-stack) line) ]
-    [(string=? curr-token "id") (parse-token token-stream (append `("term","term_tail") popped-stack) line)]
-    [(string=? curr-token "num") (parse-token token-stream (append `("term","term_tail") popped-stack) line)]
-    [(string=? curr-token "newLine") (expr (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `(" EXPR ERROR") line)]
+    [(string=? curr-token "lParen") (term_tail (term token-stream line) line)]
+    [(string=? curr-token "id") (term_tail (term token-stream line) line)]
+    [(string=? curr-token "num") (term_tail (term token-stream line) line)]
+    [(string=? curr-token "newLine") (expr (rest token-stream) (+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (term_tail token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (term_tail token-stream line)
+  (define curr-token (first token-stream))  
   (cond
-    [(string=? curr-token "add") (parse-token token-stream (append `("add_op","term","term_tail") popped-stack) line)]
-    [(string=? curr-token "minus") (parse-token token-stream (append `("add_op","term","term_tail") popped-stack) line)]
-    [(string=? curr-token "write") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "read") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "$$") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "id") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "rParen") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "newLine") (term_tail (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `("TERM TAIL ERROR") line)]
+    [(string=? curr-token "add") (term_tail (term (add_op token-stream line) line) line)]
+    [(string=? curr-token "minus") (term_tail (term (add_op token-stream line) line) line)]
+    [(string=? curr-token "write") token-stream]
+    [(string=? curr-token "read") token-stream]
+    [(string=? curr-token "$$") token-stream]
+    [(string=? curr-token "id") token-stream]
+    [(string=? curr-token "rParen") token-stream]
+    [(string=? curr-token "newLine") (term_tail (rest token-stream) (+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (term token-stream call-stack line)
+(define (term token-stream line)
   (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+  
   (cond
-    [(string=? curr-token "id") (parse-token token-stream (append `("factor","factor_tail") popped-stack) line)]
-    [(string=? curr-token "num") (parse-token token-stream (append `("factor","factor_tail") popped-stack) line)]
-    [(string=? curr-token "lParen") (parse-token token-stream (append `("factor","factor_tail") popped-stack) line)]
-    [(string=? curr-token "newLine") (term (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `(" TERM ERROR") line)]
+    [(string=? curr-token "id") (factor_tail (factor token-stream line) line)]
+    [(string=? curr-token "num") (factor_tail (factor token-stream line) line)]
+    [(string=? curr-token "lParen") (factor_tail (factor token-stream line) line)]
+    [(string=? curr-token "newLine") (term (rest token-stream)(+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (factor_tail token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (factor_tail token-stream line)
+  (define curr-token (first token-stream))  
   (cond
-    [(string=? curr-token "multiply") (parse-token token-stream (append `("mult_op","factor","factor_tail") popped-stack) line)]
-    [(string=? curr-token "divide") (parse-token token-stream (append `("mult_op","factor","factor_tail") popped-stack) line)]
-    [(string=? curr-token "plus") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "minus") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "$$") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "id") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "rParen") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "read") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "write") (parse-token token-stream popped-stack line)]
-    [(string=? curr-token "newLine") (factor_tail (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `("FACTOR TAIL ERROR") line)]
+    [(string=? curr-token "multiply") (factor_tail (factor (mult_op token-stream line) line) line)]
+    [(string=? curr-token "divide") (factor_tail (factor (mult_op token-stream line) line) line)]
+    [(string=? curr-token "add") token-stream]
+    [(string=? curr-token "minus") token-stream]
+    [(string=? curr-token "$$") token-stream]
+    [(string=? curr-token "id") token-stream]
+    [(string=? curr-token "rParen") token-stream]
+    [(string=? curr-token "read") token-stream]
+    [(string=? curr-token "write") token-stream]
+    [(string=? curr-token "newLine") (factor_tail (rest token-stream)(+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (factor token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define third-token (first(rest (rest token-stream))))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (factor token-stream line)
+  (define curr-token (first token-stream)) 
   (cond
-    [(string=? curr-token "id")(parse-token (match token-stream "id") popped-stack line)]
-    [(string=? curr-token "number") (parse-token (match token-stream "number") popped-stack line)]
-    [(string=? curr-token "lParen") (match (parse-token( match token-stream "lParen") (append `("expr") popped-stack) line) "rParen")]
-    [(string=? curr-token "newLine") (factor (rest token-stream) call-stack (+ 1 line))]
-    [else (parse-token (rest token-stream) `("FACTOR ERROR") line)]
+    [(string=? curr-token "id")(match token-stream "id" )]
+    [(string=? curr-token "num") (match token-stream "num" )]
+    [(string=? curr-token "lParen") (match (expr (match token-stream "lParen") line) "rParen" )]
+    [(string=? curr-token "newLine") (factor (rest token-stream) (+ 1 line))]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
 
-(define (add_op token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define third-token (first(rest (rest token-stream))))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (add_op token-stream line)
+  (define curr-token (first token-stream))  
   (cond
-    [(string=? curr-token "plus")(parse-token (match token-stream "plus") popped-stack line)]
-    [(string=? curr-token "minus") (parse-token (match token-stream "minus") popped-stack line)]
-    [else (parse-token (rest token-stream) `("ADD_OP ERROR") line)]
+    [(string=? curr-token "add")(match token-stream "add" )]
+    [(string=? curr-token "minus")(match token-stream "minus" )]
+    [else (error(string-append "ERROR ON LINE " (~a line))) ]
     ))
 
-(define (mult_op token-stream call-stack line)
-  (define curr-token (first token-stream))
-  (define second-token (first (rest token-stream)))
-  (define third-token (first(rest (rest token-stream))))
-  (define top-of-stack (first call-stack))
-  (define popped-stack (rest call-stack))
+(define (mult_op token-stream line)
+  (define curr-token (first token-stream)) 
   (cond
-    [(string=? curr-token "multiply")(parse-token (match token-stream "multiply") popped-stack line)]
-    [(string=? curr-token "divide") (parse-token (match token-stream "divide") popped-stack line)]
-    [else (parse-token (rest token-stream) `("MULT_OP ERROR") line)]
+    [(string=? curr-token "multiply")(match token-stream "multiply" )]
+    [(string=? curr-token "divide") (match token-stream "divide" )]
+    [else (error(string-append "ERROR ON LINE " (~a line)))]
     ))
   
-(define (match token-stream token)
+(define (match token-stream token )
   
   (cond
     [(string=? (first token-stream) "ERROR") '("ERROR",token)]
-    [(string=? (first token-stream) token) (rest token-stream)]
-    [else `("ERROR",token)]
+   ; [(string=? "$$" token) (exit)]
+    [(string=? (first token-stream) token) (rest token-stream)]    
+    [else (error "ERROR ON LINE " )]
+    ))
+
+(define (success token-stream)
+  (cond
+    [(string=? (first token-stream) "Done-Scanning") "Successfuly Parsed!!"]
+    [else (first token-stream)]
     ))
